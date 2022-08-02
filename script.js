58e692da117908b30c7dc9e156b1b3c74c9b8aab
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-02 19:45:30 -0400

    Added code for alerts asking for user input

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = true;
+var lowerCaseCharacters=true;
+var upperCaseCharacters=true;
+var numericCharacters=true;
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+generateBtn.onclick = function(){
+   var length= window.prompt("How long do you want your password to be in characters?");
+   specialCharacters=window.confirm("Do you want special characters in your password?");
+   lowerCaseCharacters=window.confirm("Do you want lower case characters in your password?");
+   upperCaseCharacters=window.confirm("Do you want upper case characters in your password?");
+   numericCharacters=window.confirm("Do you want numberic characters in your password?");
+   console.log(specialCharacters);
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
+
+// // Add event listener to generate button
+
+// generateBtn.addEventListener("click", writePassword);
+    
+
+
