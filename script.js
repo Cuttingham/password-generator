d3606aec265ab89afb0caf184c1f182547a0a775
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-02 20:04:20 -0400

    Added array of possible usable characters

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var specialCharacters = true;
 var lowerCaseCharacters=true;
 var upperCaseCharacters=true;
 var numericCharacters=true;
+var usableCharacters=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
+"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
+ "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+ "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+  "y", "z"," ","!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[",,"]","^","_","`","{","|","}","~"]
+
 
 // Write password to the #password input
 generateBtn.onclick = function(){
@@ -12,20 +18,18 @@ generateBtn.onclick = function(){
    lowerCaseCharacters=window.confirm("Do you want lower case characters in your password?");
    upperCaseCharacters=window.confirm("Do you want upper case characters in your password?");
    numericCharacters=window.confirm("Do you want numberic characters in your password?");
-   console.log(specialCharacters);
+}
+console.log(specialCharacters);
 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
+// Add event listener to generate button
 
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
-
-// // Add event listener to generate button
-
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
     
 
 
